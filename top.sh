e4e70abc3fa16bc6afd394d1b72e4ef93c2f437e
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-10-23 11:50:20 +0000

    Add option to stream top.sh output
    Usage: ./top.sh stream

--- top.sh
+++ top.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker stats --no-stream=true `docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)`
+if [ "x$1" = "xstream" ]; then
+	NO_STREAM=false
+else
+	NO_STREAM=true
+fi
+docker stats --no-stream=$NO_STREAM `docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)`
