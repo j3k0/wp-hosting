0afcc70f7cf31531406290da03e019b5ffe86647
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-11-02 18:31:11 +0000

    Set production-oriented PHP config

--- _scripts/php.ini.sh
+++ _scripts/php.ini.sh
@@ -4,4 +4,17 @@ cat << EOF
 [PHP]
 upload_max_filesize = 32M
 post_max_size = 32M
+
+display_errors = Off
+error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT
+max_input_time = 300
+output_buffering = 4096
+register_argc_argv = Off
+request_order = "GP"
+session.gc_divisor = 1000
+session.hash_bits_per_character = 5
+short_open_tag = Off
+url_rewriter.tags = "a=href,area=href,frame=src,input=src,form=fakeentry"
+variables_order = "GPCS"
+
 EOF
