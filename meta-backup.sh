1aac871ec858747c5079fc8dbb81e83d60ffdeed
Jean-Christophe Hoelt <hoelt@fovea.cc>
2016-09-28 08:25:50 +0000

    Do not backup playgrounds

--- meta-backup.sh
+++ meta-backup.sh
@@ -3,4 +3,4 @@ set -e
 cd "`dirname $0`"
 BACKUP_DIR=/backup/`hostname`/wp-hosting
 mkdir -p $BACKUP_DIR
-nice -n 19 rsync -va --bwlimit=1000 --log-file=./meta-backup.log --delete "$(pwd)"/ $BACKUP_DIR
+nice -n 19 rsync -va --bwlimit=1000 --log-file=./meta-backup.log --exclude "wp.*.playground" --delete "$(pwd)"/ $BACKUP_DIR
