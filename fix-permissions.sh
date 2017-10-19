c2b2bcfa77b64b6ff3ebdba979b2664d453f63fe
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-10-19 20:30:11 +0000

    Make sure /cache exists in the wordpress container

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -6,6 +6,7 @@
 if test -e $PROJECT/docker-compose.yml; then
     echo "$PROJECT ..."
     docker run --rm -it --volumes-from ${APPNAME}_webdata_1 --volumes-from ${APPNAME}_dbdata_1 --workdir /var/www/html --user=root haron/vim chown -cR www-data:www-data /var/www/html
+    docker exec -it ${APPNAME}_wordpress_1 sh -c "mkdir -p /cache; chown www-data:www-data /cache"
     echo "$PROJECT Done"
 else
     echo "ERROR: Project not initialized."
