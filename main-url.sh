ff9a1f370aa023a9dad0f4d27c7b136132d00ac5
Jean-Christophe Hoelt <hoelt@fovea.cc>
2018-04-17 14:22:04 +0000

    Make https the default main url

--- main-url.sh
+++ main-url.sh
@@ -5,7 +5,7 @@
 PREEXT=$(echo $DOMAIN | cut -d. -f2)
 EXT=$(echo $DOMAIN | cut -d. -f3)
 if [ "x$EXT" = "x" ] || [ ${#PREEXT} -le 2 ]; then
-    echo http://www.$DOMAIN
+    echo https://www.$DOMAIN
 else
-    echo http://$DOMAIN
+    echo https://$DOMAIN
 fi
