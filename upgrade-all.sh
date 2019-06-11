0611d40ffa7e5f619cf9e3178a425e324e83ab6a
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-06-11 13:22:16 +0000

    Update upgrade-all.sh
    Allow to select sites to upgrade in advance (no need to wait for each)

--- upgrade-all.sh
+++ upgrade-all.sh
@@ -2,6 +2,21 @@
 
 YES=$([ "x$1" = "x-y" ] || [ "x$1" == "x--yes" ] && echo "1" || echo "0")
 
+CONFIRMED_LIST=""
+
+for i in $(./ls.sh); do
+    if [ $YES == 1 ]; then
+        echo "$i..."
+        CONFIRM=y
+    elif [ "x$CONFIRM" != "xq" ]; then
+        echo "Upgrade $i? [y/n/q]"
+        read CONFIRM
+    fi
+    if [ "x$CONFIRM" = "xy" ]; then
+        CONFIRMED_LIST="$CONFIRMED_LIST $i"
+    fi
+done
+
 for i in $(cat */docker-compose.yml | grep image | cut -d: -f2 | sort | uniq); do
     echo docker pull $i
     docker pull $i
@@ -13,17 +28,12 @@ docker build -t fovea/wordpress docker
 docker pull $(cat phpmyadmin/Dockerfile | grep FROM | cut -d\  -f2)
 docker build -t fovea/phpmyadmin phpmyadmin
 
-for i in $(./ls.sh); do
-    if [ $YES == 1 ]; then
-        echo "$i..."
-        CONFIRM=y
-    else
-        echo "Upgrade $i? [y/n]"
-        read CONFIRM
-    fi
-    if [ "x$CONFIRM" = "xy" ]; then
-        ./upgrade.sh "$i"
-    fi
+echo
+echo "Let's go!"
+echo
+
+for i in $CONFIRMED_LIST; do
+    ./upgrade.sh "$i"
     if [ "x$SLEEP_BETWEEN" != "x" ]; then
         sleep "$SLEEP_BETWEEN"
     fi
