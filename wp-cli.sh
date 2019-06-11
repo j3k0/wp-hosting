b928cea4dcb656b2f0a8eb1d487e3b9086785386
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-06-11 13:22:51 +0000

    Fix wp-cli.sh

--- wp-cli.sh
+++ wp-cli.sh
@@ -11,7 +11,7 @@ if test -e $PROJECT/docker-compose.yml; then
         VOLUMES_ARG="--volumes-from ${APPNAME}_webdata_1 --volumes-from ${APPNAME}_dbdata_1"
         NETWORK_ARG="--link ${APPNAME}_db_1:mysql"
     fi
-    docker run --rm -it $NETWORK_ARG $VOLUMES_ARG -v $(pwd)/_scripts/wp:/usr/local/bin/wp -v $(which less):/usr/bin/less  --workdir /var/www/html --entrypoint wp --user=www-data wordpress --allow-root "$@"
+    docker run --rm $NETWORK_ARG $VOLUMES_ARG -v $(pwd)/_scripts/wp:/usr/local/bin/wp -v $(which less):/usr/bin/less  --workdir /var/www/html --entrypoint wp --user=www-data wordpress --allow-root "$@"
 else
     echo "ERROR: Project not initialized."
 fi
