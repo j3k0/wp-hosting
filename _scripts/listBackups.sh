9170e5907530750cb8dd7fee6e3ed5f4bebfc7de
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-06-11 13:19:17 +0000

    Fix listBackups

--- _scripts/listBackups.sh
+++ _scripts/listBackups.sh
@@ -1,6 +1,6 @@
 function listBackups() {
     echo
     echo "Available backups:"
-    ls -1 $1/backups|cut -d_ -f2|cut -d. -f1|sort|uniq
+    ls -1 /backups/$1|cut -d_ -f2|cut -d. -f1|sort|uniq
     echo
 }
