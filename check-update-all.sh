4e0eb38b8aec30d722d4b21121f77ba931ad1c56
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-06-11 13:20:06 +0000

    Update check-update-all.sh

--- check-update-all.sh
+++ check-update-all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-PROJECTS=$(./ls.sh)
+PROJECTS=$(./ls.sh --enabled)
 for PROJECT in $PROJECTS; do
     printf "%32s ... " "$PROJECT"
-    ./wp-cli.sh "$PROJECT" core check-update | grep 'Success' > /dev/null && echo OK || echo Update needed
+    TYPE="$(cat "$PROJECT/config" | grep ^TYPE= | cut -d= -f2)"
+    if [ "${TYPE:-wordpress}" = wordpress ]; then
+        ./wp-cli.sh "$PROJECT" core check-update | grep 'Success' > /dev/null && echo OK || echo Update needed
+    else
+        echo "Not wordpress: $TYPE"
+    fi
+    sleep 1
 done
