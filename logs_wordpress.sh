f8c32d4e0e8b41366429fa49c32552a109587d9e
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-06-12 06:03:28 +0000

    Update logs_wordpress.sh

--- logs_wordpress.sh
+++ logs_wordpress.sh
@@ -5,6 +5,7 @@ alias grey-grep="GREP_COLOR='2;90' grep -E --color=always --line-buffered"
 alias red-grep="GREP_COLOR='1;31' grep -E --color=always --line-buffered"
 alias green-grep="GREP_COLOR='1;32' grep -E --color=always --line-buffered"
 alias yellow-grep="GREP_COLOR='1;33' grep -E --color=always --line-buffered"
+alias violet-grep="GREP_COLOR='1;35' grep -E --color=always --line-buffered"
 alias cyan-grep="GREP_COLOR='1;36' grep -E --color=always --line-buffered"
 
 . _scripts/base.sh
@@ -19,6 +20,7 @@ if test -e $PROJECT/docker-compose.yml; then
         | cyan-grep "INFO|$" \
         | yellow-grep "WARN|$" \
         | red-grep "\[ERROR\].*|\[FATAL\].*|\[[^]]*error\].*|$" \
+        | violet-grep "\[[^]]*notice\].*|$" \
         | cyan-grep "\"(GET|POST|HEAD|PUT|OPTIONS) [^\"]+\" 3[0-9]+|$" \
         | yellow-grep "\"(GET|POST|HEAD|PUT|OPTIONS) [^\"]+\" 4[0-9]+|$" \
         | red-grep "\"(GET|POST|HEAD|PUT|OPTIONS) [^\"]+\" 5[0-9]+|$" \
