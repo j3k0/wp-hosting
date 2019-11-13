5f4253e5a175c2759c6f24eb081ac6ff8e04a936
Jean-Christophe Hoelt <hoelt@fovea.cc>
2019-11-13 18:51:43 +0000

    Better report failed image build

--- upgrade-all.sh
+++ upgrade-all.sh
@@ -23,9 +23,10 @@ for i in $(cat */docker-compose.yml | grep image | cut -d: -f2 | sort | uniq); d
 done
 
 docker pull $(cat docker/Dockerfile | grep FROM | cut -d\  -f2)
-docker build -t fovea/wordpress docker
-
 docker pull $(cat phpmyadmin/Dockerfile | grep FROM | cut -d\  -f2)
+
+set -e
+docker build -t fovea/wordpress docker
 docker build -t fovea/phpmyadmin phpmyadmin
 
 echo
