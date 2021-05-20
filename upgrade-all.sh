0c09832cf937a3fd3a42855707dad375b5897bc1
Jean-Christophe Hoelt <hoelt@fovea.cc>
2021-05-20 08:15:23 +0000

    Update upgrade-all.sh

--- upgrade-all.sh
+++ upgrade-all.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
-YES=$([ "x$1" = "x-y" ] || [ "x$1" == "x--yes" ] && echo "1" || echo "0")
-
 CONFIRMED_LIST=""
+if [ "x$1" = "x--sites" ]; then
+    shift
+    while [ "x$1" != "x" ]; do
+       CONFIRMED_LIST="$CONFIRMED_LIST $1" 
+       shift
+    done
+else
+    YES=$([ "x$1" = "x-y" ] || [ "x$1" == "x--yes" ] && echo "1" || echo "0")
+    for i in $(./ls.sh); do
+        if [ $YES == 1 ]; then
+            echo "$i..."
+            CONFIRM=y
+        elif [ "x$CONFIRM" != "xq" ]; then
+            echo "Upgrade $i? [y/n/q]"
+            read CONFIRM
+        fi
+        if [ "x$CONFIRM" = "xy" ]; then
+            CONFIRMED_LIST="$CONFIRMED_LIST $i"
+        fi
+    done
+fi
 
-for i in $(./ls.sh); do
-    if [ $YES == 1 ]; then
-        echo "$i..."
-        CONFIRM=y
-    elif [ "x$CONFIRM" != "xq" ]; then
-        echo "Upgrade $i? [y/n/q]"
-        read CONFIRM
-    fi
-    if [ "x$CONFIRM" = "xy" ]; then
-        CONFIRMED_LIST="$CONFIRMED_LIST $i"
-    fi
-done
+if [ "x$SKIP_DOCKER" != xYES ]; then
+    for i in $(cat */docker-compose.yml | grep image | cut -d: -f2 | sort | uniq); do
+        echo docker pull $i
+        docker pull $i
+    done
 
-for i in $(cat */docker-compose.yml | grep image | cut -d: -f2 | sort | uniq); do
-    echo docker pull $i
-    docker pull $i
-done
+    docker pull $(cat docker/Dockerfile | grep FROM | cut -d\  -f2)
+    docker pull $(cat phpmyadmin/Dockerfile | grep FROM | cut -d\  -f2)
 
-docker pull $(cat docker/Dockerfile | grep FROM | cut -d\  -f2)
-docker pull $(cat phpmyadmin/Dockerfile | grep FROM | cut -d\  -f2)
-
-set -e
-docker build -t fovea/wordpress docker
-docker build -t fovea/phpmyadmin phpmyadmin
+    set -e
+    docker build -t fovea/wordpress docker
+    docker build -t fovea/phpmyadmin phpmyadmin
+else
+    set -e
+fi
 
 echo
 echo "Let's go!"
@@ -38,6 +48,9 @@ for i in $CONFIRMED_LIST; do
     if [ "x$SLEEP_BETWEEN" != "x" ]; then
         sleep "$SLEEP_BETWEEN"
     fi
+    if [ "x$UPDATE_WORDPRESS" = "xYES" ]; then
+        SKIP_DOCKER=YES ./update.sh "$i"
+    fi
 done
 
-sudo service nginx reload
+# sudo service nginx reload
