0e8a63d3ce4c43f2e424d239867a803053afa88e
Jean-Christophe Hoelt <hoelt@fovea.cc>
2023-09-13 07:35:32 +0000

    Add nginx-site.sh, installs nginx config in load balancer

---
+++ nginx-site.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. _scripts/base.sh
+
+export SSL_DIR="/etc/ssl/$PROJECT"
+export CONFIG_DIR="/etc/nginx/wp-hosting"
+ssh root@wp-hosting-lb mkdir -p "$SSL_DIR" "$CONFIG_DIR"
+
+echo "Copy config and SSL files"
+rsync -r config/ "wp-hosting-lb:$CONFIG_DIR"
+rsync $PROJECT/ssl/ "wp-hosting-lb:$SSL_DIR"
+
+echo "Generate and copy nginx config"
+./_scripts/nginx-site.sh > $PROJECT/nginx-site
+scp "$PROJECT/nginx-site" root@wp-hosting-lb:/etc/nginx/sites-available/$PROJECT.conf
+ssh root@wp-hosting-lb ln -s ../sites-available/$PROJECT.conf /etc/nginx/sites-enabled/$PROJECT.conf
