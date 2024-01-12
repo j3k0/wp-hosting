0b2d871949afb826b1880eb50da4640a9c6646bc
Jean-Christophe Hoelt <hoelt@fovea.cc>
2024-01-12 07:08:05 +0000

    Add --enabled option to ls.sh

--- ls.sh
+++ ls.sh
@@ -2,20 +2,31 @@
 set -e
 cd "`dirname $0`"
 
+if [ "x$1" = "x--help" ]; then
+	echo "usage: $0 [--enabled] [PREFIX]"
+	echo
+	echo "example:"
+	echo
+	echo "  $0 --enabled wp.fovea"
+	echo "  return all enabled websites starting with wp.fovea"
+	echo
+	exit 0
+fi
+
 if [ "x$1" = "x--enabled" ]; then
     FILTER_ENABLED=1
     shift
 fi
 
-FILTER="$1"
-if [ -z $FILTER ]; then FILTER=config; fi
+PREFIX="$1"
+if [ -z $PREFIX ]; then PREFIX=wp.; fi
 
 if [ "x$FILTER_ENABLED" = "x1" ]; then
-    for i in $(ls */$FILTER|cut -d/ -f1); do
+    for i in $(ls $PREFIX*/config|cut -d/ -f1); do
         if ! grep STATE=DISABLED $i/config > /dev/null; then
             echo $i
         fi
     done
 else
-    ls */$FILTER|cut -d/ -f1
+    ls $PREFIX*/config|cut -d/ -f1
 fi
