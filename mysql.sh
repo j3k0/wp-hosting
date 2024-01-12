2d6a44c80b503d920fa95243afcabe1551472009
Jean-Christophe Hoelt <hoelt@fovea.cc>
2024-01-12 07:08:53 +0000

    Add "ROOT=1" option to mysql.sh
    Use it to login as root into a mysql server.

--- mysql.sh
+++ mysql.sh
@@ -11,7 +11,11 @@ fi
 # Check if there's a docker-compose.yml file
 if test -e $PROJECT/docker-compose.yml; then
     shift
-    docker exec -i $TTY_OPT ${APPNAME}_db_1 mysql -u admin -p${ADMIN_PASSWORD} wordpress "$@" # 2>&1 | grep -v "Warning: Using a password"
+    if [ "x$ROOT" = "x1" ]; then
+	    docker exec -i $TTY_OPT ${APPNAME}_db_1 mysql -u root -p${ROOT_PASSWORD} wordpress "$@" # 2>&1 | grep -v "Warning: Using a password"
+    else
+	    docker exec -i $TTY_OPT ${APPNAME}_db_1 mysql -u admin -p${ADMIN_PASSWORD} wordpress "$@" # 2>&1 | grep -v "Warning: Using a password"
+    fi
 else
     echo "ERROR: Project not initialized."
 fi
