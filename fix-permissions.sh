3e9017a056b926aac2c241a922b084735bfb7026
Jean-Christophe Hoelt <hoelt@fovea.cc>
2024-04-01 09:04:25 +0000

    Fix phpmyadmin sessions directory permissions

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -6,6 +6,7 @@ if test -e $PROJECT/docker-compose.yml; then
     echo "$PROJECT ..."
     docker run --rm -it -v `pwd`/${PROJECT}/volumes/html:/var/www/html -v `pwd`/${PROJECT}/volumes/mysql:/var/lib/mysql --workdir /var/www/html --user=root haron/vim chown -cR www-data:www-data /var/www/html
     docker exec -it ${APPNAME}_wordpress_1 sh -c "mkdir -p /cache; chown www-data:www-data /cache"
+    docker exec -it ${APPNAME}_phpmyadmin_1 sh -c "chown www-data:www-data /sessions"
     echo "$PROJECT Done"
 else
     echo "ERROR: Project not initialized."
