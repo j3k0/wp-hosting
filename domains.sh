85cf70a68c10df9f83879878b6dbe1a5a03c1830
Jean-Christophe Hoelt <hoelt@fovea.cc>
2024-11-26 06:32:00 +0000

    Fix domains.sh

--- domains.sh
+++ domains.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [ "x$1" = 'x--help' ]; then
+
+. "$(dirname "$0")/config/base"
+
+if [ "x$1" = 'x--help' ] || [ "x$1" = 'x-h' ] || [ "x$1" = 'x' ]; then
     echo "usage: $0 [project directory]..."
     echo
     echo "example: $0 *"
@@ -9,7 +12,7 @@ fi
 for domain in $(
 (for i in "$@"; do
     test -e "$i/nginx-site" && cat "$i/nginx-site" | grep -P '^[ \t]+server_name' | head -1
-done) | cut -d# -f1 | sed 's/server_name//g' | sed 's/;/ /g' | paste -s -d"," - | sed 's/,/ /g' | sed 's/[a-z0-9.-]*\.ggs\.ovh//g'
+done) | cut -d# -f1 | sed 's/server_name//g' | sed 's/;/ /g' | paste -s -d"," - | sed 's/,/ /g' | sed "s/[a-z0-9.-]*\.$(echo $BACKEND_DOMAIN | sed 's/\./\\./g')//g"
 ); do
     echo $domain
 done
